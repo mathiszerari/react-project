8a6de307c1b7f34cabd7e7eef41bcdf47380cc57
Val <pro.mvaldo@gmail.com>
2024-11-05 21:06:24 +0100

    feat: Router guards

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,18 @@
+import { Outlet, useNavigate } from "react-router-dom";
+import { useUserStore } from "../stores/userStore";
+import { useEffect } from "react";
+
+export default function ProtectedRoute() {
+  const checkUserAuth = useUserStore((state) => state.checkUserAuth);
+  const navigate = useNavigate();
+  const checkAuth = async () => {
+    const isAuth = await checkUserAuth();
+    if (!isAuth) {
+      navigate("/login");
+    }
+  };
+  useEffect(() => {
+    checkAuth();
+  }, []);
+  return <Outlet />;
+}
