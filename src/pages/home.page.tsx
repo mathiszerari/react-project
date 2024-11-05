d4e75a67c7d00c4a0aa8139244f80666a59affab
Val <pro.mvaldo@gmail.com>
2024-11-05 22:13:28 +0100

    feat : add current user friend list

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+import { Friend } from "../types/friends";
+import { getUserFriends } from "../services/friends.service";
+
 export default function HomePage() {
+  const [friends, setFriends] = useState<Friend[]>([]);
+
+  const fetchFriends = async () => {
+    const friendList = await getUserFriends();
+    setFriends(friendList);
+  };
+
+  const sortedByDateFriendsList = friends.sort((a, b) => {
+    return new Date(b.startedAt).getTime() - new Date(a.startedAt).getTime();
+  });
+  useEffect(() => {
+    fetchFriends();
+  }, []);
+
   return (
     <div>
       <h1>Home Page</h1>
+      <div>
+        <ul>
+          {sortedByDateFriendsList.map((friend) => (
+            <li key={friend.userId}>{friend.username}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
