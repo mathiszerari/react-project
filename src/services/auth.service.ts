f83f571b724e420defc31620740a71c0159377b5
Val <pro.mvaldo@gmail.com>
2024-11-05 21:08:50 +0100

    feat : add auth service

---
+++ src/services/auth.service.ts
@@ -0,0 +1,37 @@
+import { UserDTO } from "../types/userDto";
+
+export async function loginUser(data: UserDTO) {
+  const response = await fetch(
+    `${process.env.REACT_APP_API_BASE_URL}/auth/login`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(data),
+    }
+  );
+  if (!response.ok) {
+    const { message } = await response.json();
+    throw new Error(message);
+  }
+}
+
+export async function registerUser(data: UserDTO) {
+  const response = await fetch(
+    `${process.env.REACT_APP_API_BASE_URL}/auth/register`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(data),
+    }
+  );
+  if (!response.ok) {
+    const { message } = await response.json();
+    throw new Error(message);
+  }
+}
