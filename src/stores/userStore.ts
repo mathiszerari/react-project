b6b78c87ebf4a8c814149ac81bd99df3cdb84d18
Val <pro.mvaldo@gmail.com>
2024-11-05 16:30:28 +0100

    feat: router guard for non auth user

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -10,6 +10,7 @@ type Action = {
   clearUserId: () => void;
   updateUsername: (newUsername: string) => void;
   updateUserId: (newUserId: string) => void;
+  checkUserAuth: () => Promise<boolean>;
 };
 export const useUserStore = create<State & Action>((set) => ({
   username: "",
@@ -18,4 +19,16 @@ export const useUserStore = create<State & Action>((set) => ({
   clearUserId: () => set({ userId: "" }),
   updateUsername: (newUsername: string) => set({ username: newUsername }),
   updateUserId: (newUserId: string) => set({ userId: newUserId }),
+  checkUserAuth: async () => {
+    const response = await fetch(
+      `${process.env.REACT_APP_API_BASE_URL}/auth/me`,
+      {
+        credentials: "include",
+      }
+    );
+    if (!response.ok) {
+      return false;
+    }
+    return true;
+  },
 }));
