0d976267ca4172df3202fca21297f4a4e7c22f1d
Val <pro.mvaldo@gmail.com>
2024-11-05 21:26:44 +0100

    feat : add logout button

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -35,3 +35,17 @@ export async function registerUser(data: UserDTO) {
     throw new Error(message);
   }
 }
+
+export async function logoutUser() {
+  const response = await fetch(
+    `${process.env.REACT_APP_API_BASE_URL}/auth/logout`,
+    {
+      method: "POST",
+      credentials: "include",
+    }
+  );
+  if (!response.ok) {
+    const { message } = await response.json();
+    throw new Error(message);
+  }
+}
