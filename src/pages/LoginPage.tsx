309b2365459a3868e5395f8483cddf655f403eae
Val <pro.mvaldo@gmail.com>
2024-11-05 21:21:49 +0100

    feat : redirect user to home page after login or register

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,13 +1,19 @@
 import { useNavigate } from "react-router-dom";
 import { loginUser } from "../services/auth.service";
 import AuthForm from "../components/Form/AuthForm";
+import { UserDTO } from "../types/userDto";
 
 export default function LoginPage() {
   const navigate = useNavigate();
+
+  const onSubmit = async (data: UserDTO) => {
+    await loginUser(data);
+    navigate("/");
+  };
   return (
     <>
       <h1>Connexion</h1>
-      <AuthForm submitFn={loginUser} />
+      <AuthForm submitFn={onSubmit} />
       <button onClick={() => navigate("/register")}>
         Pas encore inscrit ?
       </button>
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,13 +1,19 @@
 import { useNavigate } from "react-router-dom";
 import { registerUser } from "../services/auth.service";
 import AuthForm from "../components/Form/AuthForm";
+import { UserDTO } from "../types/userDto";
 
 export default function RegisterPage() {
   const navigate = useNavigate();
+
+  const onSubmit = async (data: UserDTO) => {
+    await registerUser(data);
+    navigate("/");
+  };
   return (
     <>
       <h1>Inscription</h1>
-      <AuthForm submitFn={registerUser} />
+      <AuthForm submitFn={onSubmit} />
       <button onClick={() => navigate("/login")}>Déjà inscrit ?</button>
     </>
   );
