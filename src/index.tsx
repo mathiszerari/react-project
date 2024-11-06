f9642e699cbd303d5e461f5995fbb405f515d893
Mathis Zerari <mathiszerari@icloud.com>
2024-11-06 09:24:28 +0100

    rename component and page files

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import HomePage from "./pages/HomePage";
 import ChatPage from "./pages/ChatPage";
-import FriendPage from "./pages/FriendPage";
+import FriendPage from "./pages/friend-page.page";
 import App from "./App";
 import "./index.css";
 
--- src/pages/friend-page.page.tsx
+++ src/pages/friend-page.page.tsx
@@ -3,7 +3,7 @@ import FriendRequest from "../types/friend-request";
 import { dateFormater } from "../utils/dateFormater";
 import FriendRequestCard from "../components/friend-request-card";
 import { fetchFriendRequests } from "../services/friend-request.service";
-import Loader from "../components/loader/loader";
+import Loader from "../components/loader/loader.component";
 
 export default function FriendPage() {
   const [friendRequests, setFriendRequests] = useState<FriendRequest[]>([]);
