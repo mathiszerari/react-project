c3c17e16b16b127cb8e7532a46778d689815d8ae
Mathis Zerari <mathiszerari@icloud.com>
2024-11-06 11:29:05 +0100

    :sparkles: new friend request notification feature

--- src/pages/friend.page.tsx
+++ src/pages/friend.page.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import FriendRequestCard from "../components/friend-request-card.component";
-import { fetchFriendRequests } from "../services/friend-request.service";
+import { eventFetchFriendRequests, fetchFriendRequests } from "../services/friend-request.service";
 import Loader from "../components/loaders/loader.component";
 import { FriendRequest } from "../types/friend-request";
 
@@ -9,13 +9,28 @@ export default function FriendPage() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    loadFriendRequests();
-  }, []);
+    loadInitialRequests();
 
-  async function loadFriendRequests() {
-    const requests = await fetchFriendRequests();
-    setFriendRequests(requests);
-    setLoading(false);
+    const handleNewFriendRequest = (data: FriendRequest) => {
+      setFriendRequests(prevRequests => {return [data, ...prevRequests];});
+    };
+
+    const eventSource = eventFetchFriendRequests(handleNewFriendRequest);
+
+    return () => {
+      eventSource.close();
+    };
+  }, []); 
+  
+  async function loadInitialRequests() {
+    try {
+      const requests = await fetchFriendRequests();
+      setFriendRequests(requests);
+    } catch (error) {
+      console.error("Error loading friend requests:", error);
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
@@ -26,7 +41,6 @@ export default function FriendPage() {
 
       <div className="border m-6 p-4 bg-blue-100 rounded-3xl">
         <div className="border rounded-3xl p-4 bg-blue-50">
-
           <span className="mx-16 my-6 text-2xl font-bold">Friends Requests</span>
 
           {friendRequests.map((request) => (
